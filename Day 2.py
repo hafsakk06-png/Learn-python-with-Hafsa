3e048f9a8d80374ee269e621ea2a1bedecaddd0f
Hafsa kk <hafsakk06@gmail.com>
2025-09-21 18:56:14 +0500

    keep learning and keep exploring

---
+++ Day 2.py
@@ -0,0 +1,34 @@
+# Hello coders i hope you all  will fine and doing great
+# so start our day 2 with fresh mind and fresh code
+# topic of today is Variables and Data types
+# so let's start
+# Variables are used to store data in a program
+# In Python, you don't need to declare a variable before using it
+# You can simply assign a value to a variable and start using it
+
+# integer
+age = 19
+print("age :", age)
+print("Type of age:", type(age))
+
+# float
+pi = 3.12
+print("pi value :", pi)
+print("Type of pi:", type(pi))
+
+# string
+name = "Hafsa"      #you can wirte your name here
+print("Name :", name)
+print("Type of name:", type(name))
+
+# boolean
+is_student = True
+print("Is student :", is_student)
+print("Type of is_student:", type(is_student))
+
+# so my lovely coders try try to make your own variables and print them I hope you will enjoy
+# you can also change the value of a variables and see thw output
+# install visual studio code and do practice it is very helpfull to learn python
+# see you in next lecture
+# keep learning and keep exploring
+# always be happy and keep smiling

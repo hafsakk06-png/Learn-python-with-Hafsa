9a4a92c040c4e400f47ce3f4c97dcfccd9e1a51b
Hafsa kk <hafsakk06@gmail.com>
2025-09-21 18:21:38 +0500

    Learn python and ask any question

---
+++ Day 1.py
@@ -0,0 +1,13 @@
+#Hello students i am Hafsa Muskan, Welcome to python programming I hope you will enjoy learning it.
+# Day 1:Hello world in python
+
+#printing a simple message
+print("Hello begginers")
+
+#printing your name
+#My name is Hafsa Muskan you can replace it with your name
+print("My name is Hafsa Muskan")
+
+#printing a motivational quote
+print("Hard work beats talent when talent doesn't work hard.")
+

e3ae31afa57cdaf87ba1aeeb9d6fd80cd607d630
Hafsa kk <hafsakk06@gmail.com>
2025-09-28 17:23:51 +0500

    Learn and share your knowledge : )

---
+++ Day 6.py
@@ -0,0 +1,47 @@
+# Hello coders
+# Todsay we are going to learn Strings and strings methods in python.
+#              STRINGS
+# Strings are a collection of characters enclosed in single quotes(' ') or double quotes(" ").
+
+name = "Hafsa"
+city = 'Mirpurkhas'
+
+#            STRING INDEXING
+# In python, strings are indexed. The first character of the string has index 0, the second character has index 1, and so on.
+# You can access individual characters of a string using their index.
+
+word = "Python"
+print(word[0])   # P
+print(word[1])   # y
+print(word[-1])  # n (last character)
+
+#            STRINGS SLICING
+
+text = "Hello World"
+print(text[0:5])   # Hello
+print(text[6:])    # World
+print(text[:5])    # Hello
+
+#         STRING METHODS
+
+msg = " i love python "
+
+print(msg.upper())      # I LOVE PYTHON
+print(msg.lower())      # i love python
+print(msg.strip())      # removes extra spaces
+print(msg.replace("python", "C++"))  # i love C++
+print(msg.split())      # ['i', 'love', 'python']
+
+#         STRING + NUMBERS(CONCATENATION)
+
+name = "Hafsa"
+age = 19
+print(name + " is " + str(age) + " years old.")
+
+# so this is all for today. see you in the next class.
+# Thank you!
+# Have a nice day!
+# keep Learning and keep exploring.
+# Happy coding!
+# Love you all.
+# Bye Bye!

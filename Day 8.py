5a2e86ecd2395580d1c4a4803490f7aa3db44697
Hafsa kk <hafsakk06@gmail.com>
2025-09-30 22:43:20 +0500

    keep exploring : )

---
+++ Day 8.py
@@ -0,0 +1,55 @@
+# Hello everyone
+# I hope you all are trying to doing good
+# so my coders today is our 8th day
+# so let we start
+
+#  Loops and patterns
+#   For Loop
+
+# print numbers from 1 to 5
+for i in range(1, 6):
+    print(i)
+
+#    While Loop
+
+# print numbers from 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+#   Nested Loop-Print a square pattern
+
+# 5x5 star square
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+
+#    Triangle pattern
+
+# Right angled triangle
+for i in range(1, 6):
+    print("*" * i)
+
+#    Number pattern
+
+# Triangle of numbers
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+
+
+#   so my students I have a homework for you try and solve problems and follow me and reply me
+
+#Print a table of any number (e.g., table of 7).
+
+#Make a pyramid pattern with *.
+
+#Print sum of first 10 natural numbers using a loop.
+
+#Have fun.Happy coding
+#Best of luck
+# Keep smiling and always be happy :)
+# byeeeeee :)

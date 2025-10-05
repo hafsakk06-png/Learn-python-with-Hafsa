416683251a2b9209718a37a85c63580fac272743
Hafsa kk <hafsakk06@gmail.com>
2025-10-05 15:16:53 +0500

    Try to complete your tasks : )

---
+++ Day 11.py
@@ -0,0 +1,52 @@
+# Hello coders
+# so today is our day 11
+# I hope you all are doing great so lets start our coding
+
+#Functions in Python (Part 1)
+#What is a Function?
+
+# A function is a block of code that runs only when it is called.
+# It helps you avoid repeating code and makes your program organized.
+
+#Function with Parameters
+
+# You can pass data into a function using parameters.
+
+def greet(name):
+    print("Hello", name, "! How are you?")
+
+greet("Hafsa")
+greet("Muskan")
+
+#Function with Return Value
+
+# Functions can return data using the return keyword.
+
+def add(a, b):
+    return a + b
+
+sum = add(5, 7)
+print("Sum:", sum)
+
+# Default Parameters
+
+# You can set a default value for parameters.
+
+def greet(name="Student"):
+    print("Hello", name)
+
+greet()
+greet("Hafsa")
+
+# so my genius coders I have a task for you
+
+# Make a function multiply(x, y) that returns the product.
+
+# Make a function is_even(num) that returns True if number is even, else False.
+
+#Make a function calculate_square(n) that prints the square of numbers from 1 to n.
+
+# Happy Coding and try to complete your task
+# Keep learning and keep exploring.
+# Love you all 
+# byeeeeee

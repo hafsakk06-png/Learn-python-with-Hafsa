3deff087c88f5d9e518e8447d0365159326323d5
Hafsa kk <hafsakk06@gmail.com>
2025-10-21 21:03:13 +0500

    Day 15 with more knowledge ; )

--- Day 15.py
+++ Day 15.py
@@ -26,3 +26,37 @@ intro(age=20, name="Hafsa")
 
 def calculator(a, b):
     return a + b, a -
+
+    #*args and kwargs
+
+#These allow you to pass a variable number of arguments to a function.
+
+def add_all(*args):
+    return sum(args)
+
+print(add_all(1, 2, 3, 4))  # Output: 10
+
+
+# *args → collects multiple positional arguments into a tuple.
+
+def show_details(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+show_details(name="Hafsa", age=20, city="Lahore")
+
+
+# **kwargs → collects multiple keyword arguments into a dictionary.
+
+# Lambda Functions
+
+# Lambda = small, single-line anonymous function.
+
+square = lambda x: x * x
+print(square(5))
+
+
+# Same as:
+
+def square(x):
+    return x * x
